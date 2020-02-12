e946ac46a5c63d8454cb398a213adfbc45efc784
Frank Kohn <frank@sellhound.com>
2020-02-12 13:58:37 -0800

    Updates

---
+++ precache-manifest.381933ea0ba65672296df5a5c75688d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "450ca69f5bc1f849bbc45eea03df74ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6dc4b82ca1625c759fe",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6b03c5c47a571f889ead",
+    "url": "/static/js/2.a16312d8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.a16312d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6dc4b82ca1625c759fe",
+    "url": "/static/js/main.01d8c965.chunk.js"
+  },
+  {
+    "revision": "00763e12e1a860b091bb",
+    "url": "/static/js/runtime-main.0f41a5ee.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
